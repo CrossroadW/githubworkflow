a3de5d6cdd3af27a75ec8fa7905368fdb6ed0a6c
awesome <3109372170@qq.com>
2025-11-12 15:53:31 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-    
-    return 0;
+#include <QApplication>
+#include <QWidget>
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+  QWidget w;
+  w.show();
+  return app.exec();
 }
